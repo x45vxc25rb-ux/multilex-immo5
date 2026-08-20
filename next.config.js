92ca071ad1663e9caa1198ae2e7636eef06b2e7b
William Van riemsdijk <w.v.r@STORM-MacBook-Air.local>
2026-08-20 13:32:56 +0200

    Increase static page generation timeout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 120,
   images: {
     // Разрешённые внешние источники изображений можно добавить сюда,
     // если фотографии объектов будут храниться не локально, а на CDN.
